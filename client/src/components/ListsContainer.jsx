2ad6e78df9d2b315f4798fc5a3051d8559304edd
SuperSus <nickvasilevski35@gmail.com>
2020-03-03 14:58:24 +0300

    client: extract list component

--- client/src/components/ListsContainer.jsx
+++ client/src/components/ListsContainer.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+import List from './ List';
+
 class ListsContainer extends Component {
   constructor(props) {
     super(props);
@@ -24,14 +26,9 @@ class ListsContainer extends Component {
   render() {
     return (
       <div className="lists-container">
-        {this.state.lists.map(list => {
-          return (
-            <div className="single-list" key={list.id}>
-              <h4>{list.title}</h4>
-              <p>{list.excerpt}</p>
-            </div>
-          );
-        })}
+        {this.state.lists.map(
+          list => <List list={list} key={list.id}/>
+        )}
       </div>
     );
   }
