513138f94f246d439a24c5a65307b79e0ed59bcf
SuperSus <nickvasilevski35@gmail.com>
2020-03-03 13:02:49 +0300

    wrap controllers by api namesoace

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,51 +1,58 @@
-class ListsController < ApplicationController
-  before_action :set_list, only: [:show, :update, :destroy]
+# frozen_string_literal: true
 
-  # GET /lists
-  def index
-    @lists = List.all
+module API
+  module V1
+    class ListsController < ApplicationController
+      before_action :set_list, only: %i[show update destroy]
 
-    render json: @lists
-  end
+      # GET /lists
+      def index
+        @lists = List.all
 
-  # GET /lists/1
-  def show
-    render json: @list
-  end
+        render json: @lists
+      end
 
-  # POST /lists
-  def create
-    @list = List.new(list_params)
+      # GET /lists/1
+      def show
+        render json: @list
+      end
 
-    if @list.save
-      render json: @list, status: :created, location: @list
-    else
-      render json: @list.errors, status: :unprocessable_entity
+      # POST /lists
+      def create
+        @list = List.new(list_params)
+
+        if @list.save
+          render json: @list, status: :created, location: @list
+        else
+          render json: @list.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /lists/1
+      def update
+        if @list.update(list_params)
+          render json: @list
+        else
+          render json: @list.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /lists/1
+      def destroy
+        @list.destroy
+      end
+
+      private
+
+      # Use callbacks to share common setup or constraints between actions.
+      def set_list
+        @list = List.find(params[:id])
+      end
+
+      # Only allow a trusted parameter "white list" through.
+      def list_params
+        params.require(:list).permit(:title, :excerpt, :description, :upvotes)
+      end
     end
   end
-
-  # PATCH/PUT /lists/1
-  def update
-    if @list.update(list_params)
-      render json: @list
-    else
-      render json: @list.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /lists/1
-  def destroy
-    @list.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_list
-      @list = List.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def list_params
-      params.require(:list).permit(:title, :excerpt, :description, :upvotes)
-    end
 end
